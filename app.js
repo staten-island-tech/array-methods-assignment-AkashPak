e4f9b8976e27eabc802ab3cdf2dd4bbc0f5821c9
AkashPak <akashp8@nycstudents.net>
2023-10-11 09:26:14 -0400

    sSQ

---
+++ app.js
@@ -0,0 +1,11 @@
+const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+
+
+
+shopping[0] = "oblock";
+console.log(shopping);
+// shopping will now return [ "tahini", "milk", "cheese", "hummus", "noodles" ]
+
+const birds = ["Parrot", "Falcon", "Owl"];
+console.log(birds.indexOf("Owl")); //  2
+console.log(birds.indexOf("marco")); // -1
