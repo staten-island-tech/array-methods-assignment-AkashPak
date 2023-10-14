c3964c2dea946921714d71064f79122d5b17efa2
AkashPak <144455859+AkashPak@users.noreply.github.com>
2023-10-13 22:16:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,38 @@ array1.forEach((element) => console.log(element));
 // Expected output: "a"
 // Expected output: "b"
 // Expected output: "c"
+
+
+const Parrots = {
+language: "Surroundings",
+color: ["Yellow", "Green", "Blue", "Purple", "Red", "Orange", "Pink"], 
+Fly: "true",
+UsuallyEaten:"No",
+Goat: "No",
+}
+
+const Penguins = {
+language: "Penguinanian",
+color: ["Black", "White"],
+Fly:"false",
+UsuallyEaten:"No",
+Goat:"No",
+}
+
+const Duck = {
+Language: "Quack",
+color: ["Yellow", "Green", "White", "Brown"],
+Fly:"True",
+UsuallyEaten:"Yes",
+Goat:"No",
+}
+const Toucan= {
+    language: "Kakaw",
+    color:["Yellow", "Red","Blue", "Orange", "Purple", "Green", "Rainbow" ],
+    Fly: "True",
+    UsuallyEaten:"No",
+    Goat:"Fruit Loops",
+}
+
+const Birds= [Parrots,Penguins,Duck,Toucan]
+Birds.forEach((yum) => console.log(yum));
